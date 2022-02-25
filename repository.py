8880fdcbbb3b6f31a71bd80ce474edb14db85283
dmsergio <sergiodm.1989@gmail.com>
2022-02-25 20:48:30 +0100

    [ADD] Repository pattern added and its tests

--- repository.py
+++ repository.py
@@ -12,3 +12,18 @@ class AbstractRepository(abc.ABC):
     @abc.abstractmethod
     def get(self, reference: str) -> Batch:
         raise NotImplementedError
+
+
+class SQLAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch: Batch):
+        self.session.add(batch)
+
+    def get(self, reference: str) -> Batch:
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
